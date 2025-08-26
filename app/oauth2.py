ad43fd1735e0af11696dcebb63f93475277f0523
Shady776 <temitayosalisu42@gmail.com>
2025-08-26 16:07:47 +0100

    new

--- app/oauth2.py
+++ app/oauth2.py
@@ -35,7 +35,7 @@ def verify_access_token(token: str, credentials_exception):
     
         if id is None:
             raise credentials_exception
-        token_data = schemas.TokenData(id=str(id))
+        token_data = schemas.TokenData(id=id)
     except JWTError:
         raise credentials_exception
     
--- app/schemas.py
+++ app/schemas.py
@@ -38,7 +38,7 @@ class Token(BaseModel):
     token_type: str
     
 class TokenData(BaseModel):
-    id: Optional[str] = None
+    id: Optional[int] = None
 
 
 class Space(BaseModel):
